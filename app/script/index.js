a7ed05a621699ca0a3b551ec726a54fbdeb21885
Ulya Habiburrahman <49488781+UlyaHr@users.noreply.github.com>
2021-03-16 19:52:24 +0700

    Update index.js

--- app/script/index.js
+++ app/script/index.js
@@ -1,8 +1,8 @@
-import { Header } from "Header.js";
-import { SectionObs } from "SectionObs.js";
-import { DarkMode } from "DarkMode.js";
-import { ContactForm } from "ContactForm.js";
-import { About } from "About.js";
+import { Header } from "/Header.js";
+import { SectionObs } from "/SectionObs.js";
+import { DarkMode } from "/DarkMode.js";
+import { ContactForm } from "/ContactForm.js";
+import { About } from "/About.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   Header();
